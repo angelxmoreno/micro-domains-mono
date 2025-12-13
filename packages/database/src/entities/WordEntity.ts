d21df95d5ffa734033e01617739496cdab655494
Angel S. Moreno <angelxmoreno@gmail.com>
2025-12-13 14:02:36 -0500

    refactor(database): updated WordEntity to drop dubious columns

--- packages/database/src/entities/WordEntity.ts
+++ packages/database/src/entities/WordEntity.ts
@@ -11,16 +11,4 @@ export class WordEntity extends AppEntity {
 
     @Column('varchar', { length: 255, nullable: false })
     source: string;
-
-    @Column('varchar', { length: 50, nullable: true })
-    partOfSpeech?: string; // Using partOfSpeech for consistency with TS naming
-
-    @Column('varchar', { length: 100, nullable: true })
-    origin?: string;
-
-    @Column('varchar', { length: 100, nullable: true })
-    location?: string;
-
-    @Column('text', { nullable: true })
-    definition?: string;
 }
